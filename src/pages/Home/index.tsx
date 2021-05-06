cffdf0fdca194c33377cdd1a2c4eef2a77d11c3c
ViniciusFloriano <viniciusfloriano05@gmail.com>
2021-05-06 20:21:28 -0300

    Started Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import CertifiedImage from '../../assets/icons/icon-certified.svg';
+import PlayImageClean from '../../assets/icons/icon-playlists-first.svg';
+import PlayImage from '../../assets/icons/icon-playlists.svg';
+import TranscriptionImage from '../../assets/icons/icon-transcription.svg';
+import PersonImage from '../../assets/images/person.png';
 import Container from '../../components/Container';
-import Heading from '../../components/Heading';
+// import Heading from '../../components/Heading';
+import HeaderContainer from './styles';
 
 const Home: React.FC = () => (
-  <Container>
-    <Heading>Alo</Heading>
-  </Container>
+  <HeaderContainer>
+    <div id="background-icons">
+      <img alt="Video" src={PlayImageClean} />
+      <img alt="Video" src={PlayImageClean} />
+      <img alt="Video" src={PlayImage} />
+      <img alt="Video" src={CertifiedImage} />
+      <img alt="Video" src={TranscriptionImage} />
+    </div>
+    <Container>
+      <div>
+        <h3>PLATAFORMA ALL IN ONE</h3>
+      </div>
+      <img alt="person" src={PersonImage} />
+    </Container>
+  </HeaderContainer>
 );
 
 export default Home;
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled('div')`
+  width: 100%;
+  height: 100vh;
+  max-height: 900px;
+  background: linear-gradient(79.95deg, #5f41d9 2.25%, #41b5d9 100.27%);
+
+  #background-icons {
+    z-index: 1;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    left: 0;
+    top: 50px;
+    height: 50px;
+    width: 100vw;
+
+    img {
+      position: absolute;
+      height: 88.5px;
+      width: 88.5px;
+    }
+
+    img:nth-child(1) {
+      top: -5px;
+      left: 5%;
+    }
+
+    img:nth-child(2) {
+      top: -90px;
+      left: 25%;
+      transform: rotate(-14.5deg);
+      height: 70.5px;
+      width: 70.5px;
+    }
+
+    img:nth-child(3) {
+      top: -5px;
+      right: 6%;
+    }
+
+    img:nth-child(4) {
+      top: 25px;
+      left: 45%;
+    }
+
+    img:nth-child(5) {
+      top: -35px;
+      right: 31%;
+      height: 70.5px;
+      width: 70.5px;
+    }
+  }
+
+  div {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    img {
+      align-self: flex-end;
+    }
+  }
+`;
+
+export default HeaderContainer;
